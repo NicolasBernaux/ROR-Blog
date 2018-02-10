67fffd3d34c8b8b8fbe6a097a13ef75f3aa08281
NicolasBernaux <bernaux.n@gmail.com>
2018-02-10 15:04:17 +0100

    :tada: First commit

---
+++ blog/app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+
+  validates :name, presence: true
+
+  has_many :comments
+end
---
+++ blog/db/migrate/20180210000324_add_comment_to_posts.rb
@@ -0,0 +1,6 @@
+class AddCommentToPosts < ActiveRecord::Migration[5.1]
+  def change
+
+    add_reference :posts, :comment, foreign_key: true
+  end
+end
